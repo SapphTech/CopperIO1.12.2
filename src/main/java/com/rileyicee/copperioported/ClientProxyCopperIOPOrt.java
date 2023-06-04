489e55467a7fc1597a0c9299a5fd383c0775fc0c
hankb <hankb@hankbear07-atlas.nord>
2023-06-04 07:27:58 -0500

    Textures Added

---
+++ src/main/java/com/rileyicee/copperioported/ClientProxyCopperIOPOrt.java
@@ -0,0 +1,26 @@
+package com.rileyicee.copperioported;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.obj.OBJLoader;
+
+public class ClientProxyCopperIOPOrt implements IProxyCopperIOPOrt {
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		OBJLoader.INSTANCE.addDomain("copperioport1122");
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
---
+++ src/main/java/com/rileyicee/copperioported/ServerProxyCopperIOPOrt.java
@@ -0,0 +1,24 @@
+package com.rileyicee.copperioported;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public class ServerProxyCopperIOPOrt implements IProxyCopperIOPOrt {
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
